2e2122797137c28c3413332f68e833b174187658
kooky-waterfall-9790 <akimclerge@gmail.com>
2019-09-30 17:04:37 +0000

    Automatically backed up by Learn

--- lib/third_challenge.rb
+++ lib/third_challenge.rb
@@ -2,18 +2,12 @@ def third_challenge
   epic_tragedy = {
    :montague => {
       :patriarch => {
-        :name => "Lord Montague",
-        :age => "53"
         #your key/value pairs here
         },
       :matriarch => {
-        :name => "Lady Montague",
-      :age => "54"
         #your key/value pairs here
         },
       :hero => {
-        :name => "Romeo",
-      :age => "15"
         #your key/value pairs here
         },
       :hero_friends => []
